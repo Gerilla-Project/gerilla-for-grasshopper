8851903bd864d3081fd699416b0f72273a871864
balbano <balbano@gmail.com>
2013-06-29 12:42:44 -0500

    Added comments.
    Added comments to BatchFiller.vb and ePlusResults.vb

--- BatchFiler.vb
+++ BatchFiler.vb
@@ -12,78 +12,73 @@ Imports Rhino.Geometry
 Imports System.IO
 
 Public Class BatchFiler
-		Inherits GH_Component	
+	'The EnergyPlus Simulation component accepts the weather file and a boolean that sets up the batch file and runs the simulation.
+	'NOTE: I don't think this needs to be its own component, and it could be rolled into the main IDFCompiler component. - Brendan 2013-06-29.
+	Inherits GH_Component
 		
-	''Constructor
+	'Constructor
     Public Sub New()
     	MyBase.New("EnergyPlus Simulation","Simulate","Initiate EnergyPlus Simulation", "Gerilla","Simulation")
     End Sub
- 
+    
+    'Guid
     Public Overrides ReadOnly Property ComponentGuid As System.Guid
     	Get
-    		Return New Guid("22B1F0E8-A8BA-414B-B2DF-D10B0CEF905F")  ''www.createguid.com
- 
+    		Return New Guid("22B1F0E8-A8BA-414B-B2DF-D10B0CEF905F")  ''www.createguid.com 
         End Get
     End Property
     
+    'Icon
     Protected Overrides ReadOnly property icon As System.drawing.Bitmap
  		Get
     		Return gerillaResource.GerillaIconSimulate
  	    End Get
 	End Property
+	
+	'Inputs
+	Protected Overrides Sub RegisterInputParams(ByVal pManager As Grasshopper.Kernel.GH_Component.GH_InputParamManager)
+		'Input 0: boolean to run the solution. Input 1: weather file file path.
+		pManager.AddBooleanParameter("Run EnergyPlus Simulation","Run","Simulate Output From IDF Compiler", GH_ParamAccess.item, False)
+    	pManager.AddTextParameter("Weather File Path","Weather File Path","Input Full File Path For Weather File Without The File Extension; ie. c:\MyDocuments\Gerilla\Weatherfiles\NewYorkTMY3.epw", GH_ParamAccess.item)   	
+	End Sub
     
-    Protected Overrides Sub RegisterInputParams(ByVal pManager As Grasshopper.Kernel.GH_Component.GH_InputParamManager)
-    	pManager.AddBooleanParameter("Run EnergyPlus Simulation","Run","Simulate Output From IDF Compiler", GH_ParamAccess.item, False)
-    	pManager.AddTextParameter("Weather File Path","Weather File Path","Input Full File Path For Weather File Without The File Extension; ie. c:\MyDocuments\Gerilla\Weatherfiles\NewYorkTMY3.epw", GH_ParamAccess.item)
-    	'pManager.AddTextParameter("Output File Path","W","Input folder file path for simulation results; ie. c:\MyDocuments\Gerilla\myProject\Outputs\", "C:\EnergyPlusV8-0-0\Outputs\" ,GH_ParamAccess.item)
-    	'pManager.AddTextParameter("IDF Project Name", "I","Input Project Name for IDF",GH_ParamAccess.item)
-    End Sub
- 
-    Protected Overrides Sub RegisterOutputParams(ByVal pManager As Grasshopper.Kernel.GH_Component.GH_OutputParamManager)
-    	'pManager.AddTextParameter("Batch File", "B", "Batch file that is executed")
-    	
-    End Sub
- 
- 	Protected Overrides Sub SolveInstance(ByVal DA As Grasshopper.Kernel.IGH_DataAccess)
- 		
+    'Outputs
+	Protected Overrides Sub RegisterOutputParams(ByVal pManager As Grasshopper.Kernel.GH_Component.GH_OutputParamManager)    	
+		'No outputs	
+	End Sub
+	
+	'Solve
+	Protected Overrides Sub SolveInstance(ByVal DA As Grasshopper.Kernel.IGH_DataAccess)
+		
+		'Get the inputs.
  		Dim run As Boolean
- 		'Dim projectName As String = nothing
  		Dim weatherPath As String = Nothing
- 		Dim outputPath As String = (GerillaProjectSetup.PublicProjectFilePath & "\" & GerillaProjectSetup.PublicProjectName & "\Results\" & GerillaProjectSetup.PublicProjectName)
  		
- 		Dim batchFile As String
- 		
- 		If (Not DA.GetData(0, run)) Then Return
- 		'If (Not DA.GetData(1, projectName)) Then Return
+  		If (Not DA.GetData(0, run)) Then Return
  		If (Not DA.GetData(1, weatherPath)) Then Return
  		
- 			
- 		batchFile = My.Computer.FileSystem.ReadAllText("C:\Gerilla\gerillaBatchTemplate.bat")
+ 		'Assemble the output path from the Gerilla Project Information component.
+ 		Dim outputPath As String = (GerillaProjectSetup.PublicProjectFilePath & "\" & GerillaProjectSetup.PublicProjectName & "\Results\" & GerillaProjectSetup.PublicProjectName)
  		
+ 		'Read the batch file template.
+ 		Dim batchFile As String = My.Computer.FileSystem.ReadAllText("C:\Gerilla\gerillaBatchTemplate.bat") 		 		
+ 		
+ 		'Add the appropriate file paths to the batch file template.
  		batchFile = Replace(batchFile, "=%~1", "=C:\EnergyPlusV8-0-0\" & GerillaProjectSetup.PublicProjectName)
  		batchFile = Replace(batchFile, "=%~2", "=C:\EnergyPlusV8-0-0\" & GerillaProjectSetup.PublicProjectName)
- 		'batchFile = Replace(batchFile, "=%~1", "=" & GerillaProjectSetup.PublicProjectFilePath & "\" & GerillaProjectSetup.PublicProjectName)
- 		'batchFile = Replace(batchFile, "=%~2", "=" & GerillaProjectSetup.PublicProjectFilePath & "\" & GerillaProjectSetup.PublicProjectName)
  		batchFile = Replace(batchFile, "=%~4", "=" & weatherPath)
  		batchFile = Replace(batchFile, "=%~~10", "=" & outputPath)
- 		
-
         
-        'DA.SetData(0, batchFile)
-        
- 		If run = True Then
- 			
- 			'Dim newBatch As New StreamWriter("C:\EnergyPlusV8-0-0\newGerillaEPLRun.bat")
- 			'Dim newBatch As New StreamWriter(GerillaProjectSetup.PublicProjectFilePath & "\" & GerillaProjectSetup.PublicProjectName & "\" & GerillaProjectSetup.PublicProjectName & "Run.bat")
+ 		If run Then
+ 			'Write out the batch file and run it.
  			Dim newBatch As New StreamWriter("C:\EnergyPlusV8-0-0\" & GerillaProjectSetup.PublicProjectName & "Run.bat")
  			newBatch.Write(batchFile)
  			newBatch.Close
  	
-' 			System.Diagnostics.Process.Start("C:\EnergyPlusV8-0-0\newGerillaEPLRun.bat")
  			System.Diagnostics.Process.Start("C:\EnergyPlusV8-0-0\" & GerillaProjectSetup.PublicProjectName & "Run.bat")
  	
  		End If
- 	
- 		
-	 	End Sub
+ 	 		
+	End Sub
+	
 End Class
--- ePlusResults.vb
+++ ePlusResults.vb
@@ -11,8 +11,10 @@ Imports Grasshopper.Kernel.Types
 Imports Rhino.Geometry
 
 Public Class ePlusResults
+	'The ePlusResults class is used by the EnergyPlus Results component (defined by the GerillaResults class) to extract the heating and cooling loads from the .eso results file. 
 	
 	Public Sub GetResults(ByVal resultsFilePath As String, firstIntegerTag As Integer, secondIntegerTag As Integer, stepSize As Integer, resultsOut As Grasshopper.DataTree(Of String))
+		'Returns the monthly heating and cooling loads for each zone as a Grasshopper Data Tree. 
 		'firstIntegerTag and secondIntegerTag are the tags that begin the lines of the first and second zone results of a particular type in the .eso.
 		'stepSize is the difference between the tags that begin the 3rd and higher lines.
 		'For example, Zone1 Heating tags for firstIntergerTag = 7, secondIntergerTag = 80, and stepSize = 33 would be: 7, 80, 113, 146, 179, etc.
@@ -22,43 +24,58 @@ Public Class ePlusResults
     	Dim resultsLine As String = Nothing
     	Dim resultsReader As System.IO.StreamReader
     	
-    	Dim numZones As Integer = 1
+    	Dim numZones As Integer = 1  	
+    	
     	For i As Integer = 0 To 99
-    		'Counts the number of zones by counting the number of times the IntegerTag appears (incrementing the tag by stepSize as it counts)
-    		'the counter begins at the secondIntegerTag and accounts for the firstIntegerTag by starting at numZones=1.    		
+    		'Counts the number of zones by counting the number of times the IntegerTag appears (incrementing the tag by stepSize*i)
+    		'the counter begins at the secondIntegerTag and accounts for the firstIntegerTag by starting at numZones=1.
+    		
+    		'NOTE: This will always run 100 times, it should be rewritten to loop until the IntegerTag is not found, that way it only runs once for each zone. -Brendan 2013-06-29
+    		
     		resultsReader = System.IO.File.OpenText(resultsFilePath)
     		Dim loopEnd As Boolean = False
+    		
     		Do While resultsReader.Peek()>= 0 And loopEnd = False
-				resultsLine = resultsReader.ReadLine
+    			'Reads results line by line looking for a line beginning with the appropriate integer tag. Loop ends when it finds one or reaches the end of the file.    			  			
+    			resultsLine = resultsReader.ReadLine
 				Dim intTest As Integer = secondIntegerTag + stepSize*i
-    			If resultsLine.Substring(0,intTest.ToString.Length) = intTest.ToString Then
+				
+				If resultsLine.Substring(0,intTest.ToString.Length) = intTest.ToString Then
     				numZones = numZones+1
-    				loopEnd = True
-    			End If
+    				loopEnd = True				
+				End If
+				
     		Loop
-    		resultsReader.Close   	
-		Next     	
-
-		resultsReader = System.IO.File.OpenText(resultsFilePath)
+    	
+    		resultsReader.Close
+    		
+    	Next
+    	
+    	'Create GH_Path object for Zone1Results.
+		resultsReader = System.IO.File.OpenText(resultsFilePath)		
 		Dim zone1ResultsPath As New Data.GH_Path(1)
+		
 		Do While resultsReader.Peek()>= 0
-			'Gets the results for the line beginning with the firstIntegerTag.
-    		resultsLine = resultsReader.ReadLine
+			'Loops through the results file searching for a line beginning with the firstIntegerTag, parses that data, and adds it to the resultsOut Grasshopper DataTree.			
+			resultsLine = resultsReader.ReadLine
+			
     		Dim stringTest As String = firstIntegerTag.ToString & ","
     		Dim resultsLineString As String = resultsLine.Substring(0, stringTest.Length)
-    		If resultsLine.Substring(0,resultsLineString.Length) = stringTest And resultsLine.Substring(resultsLineString.Length,2) <> "9," Then 
-    			'I think the <> "9," part is supposed to filter out the lines in the Data Dictionary.
-    			'This method appears to be unreliable, as it had to be changed from'<> "7,"' to '<> "9,"' to get it to work on my (Brendan's) machine
-    			'changing to a method that uses the fact that the data dictionary always ends with "End of Data Dictionary" seems more reliable.
+    		
+    		If resultsLine.Substring(0,resultsLineString.Length) = stringTest And resultsLine.Substring(resultsLineString.Length,2) <> "9," Then
+    			'NOTE: The <> "9," part filters out the lines in the Data Dictionary.    			
+    			'This method appears to be unreliable, as it had to be changed from'<> "7,"' to '<> "9,"' to get it to work when updating from EnergyPlusV6 to EnergyPlusV8.
+    			'Changing to a method that uses the fact that the data dictionary always ends with "End of Data Dictionary" seems like it would be more reliable. - Brendan 2013-06-29    			
  				Dim resultsArr() as String = Split(resultsLine, ",")
-    			resultsOut.Add(resultsArr(1).Trim(), zone1ResultsPath)
+ 				resultsOut.Add(resultsArr(1).Trim(), zone1ResultsPath)   	 				
     		End If
-    	Loop
-    	
+    		
+    	Loop    	
     	resultsReader.Close
     	
+    	'Get the results for the remaining zones.
     	For i As Integer = 1 To numZones-1
-    		'Gets the results for lines beginning with the secondIntegerTag and beyond, incremented by step size.
+    		'Gets the results for lines beginning with the secondIntegerTag and beyond, incremented by step size, parses that data, and adds it to the resultsOut Grasshopper DataTree.
     		Dim resultsPath As New Data.GH_Path(i+1)
     		Dim intTest As Integer = secondIntegerTag + stepSize*(i-1)
     		Dim intTestString As String = intTest.ToString & ","
@@ -75,9 +92,11 @@ Public Class ePlusResults
     		Loop
     		resultsReader.Close	
     	Next
+    	
 	End Sub
 	
 	Public Sub GetDistrictResults(ByVal resultsFilePath As String, firstIntegerTag As Integer, resultsOut As Grasshopper.DataTree(Of String))
+		'NOTE: District Results are not currently used in Gerilla. -Brendan 2013-06-29.
 		
 		resultsFilePath = resultsFilePath & ".mtr"
     	Dim resultsLine As String = Nothing
@@ -100,9 +119,6 @@ Public Class ePlusResults
 '    		resultsReader.Close   	
 '		Next     	
     	
-    	
-    	
-
 		resultsReader = System.IO.File.OpenText(resultsFilePath)
 		Dim districtResultsPath As New Data.GH_Path(1)
 		Do While resultsReader.Peek()>= 0
@@ -132,5 +148,6 @@ Public Class ePlusResults
 '    		Loop
 '    		resultsReader.Close	
 '    	Next
-    End Sub
+	End Sub
+	
 End Class
